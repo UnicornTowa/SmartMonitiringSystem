7ed0c78b4d9a82b603db0a959c49d9b4c2fe9c6e
UnicornTowa <90133563+UnicornTowa@users.noreply.github.com>
2022-10-30 19:01:59 +0300

    Update newmain.py

--- border_extract/newmain.py
+++ border_extract/newmain.py
@@ -6,7 +6,6 @@ execution_path = os.getcwd()
 
 cam = cv2.VideoCapture(0)
 
-
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
@@ -15,15 +14,14 @@ detector.loadModel(detection_speed="flash")
 custom = detector.CustomObjects(person=True)
 
 
-
 def per_second(second_number, output_arrays, count_arrays, average_output_count):
     f = open(os.path.join(os.getcwd(), "output", "second" + str(second_number) + ".txt"), 'w')
     for frame in output_arrays:
         for person in frame:
-            print("DETECTED")
             for coord in person["box_points"]:
                 f.write(str(coord) + ' ')
-            f.write('%')
+            # f.write('%')
+            break
         f.write('\n')
     f.close()
 
@@ -38,4 +36,6 @@ detector.detectObjectsFromVideo(camera_input=cam, custom_objects=custom,
                                 save_detected_video=False,
                                 frames_per_second=5,
                                 per_second_function=per_second,
-                                minimum_percentage_probability=50)
+                                minimum_percentage_probability=70
+                                # , detection_timeout=10
+                                )
